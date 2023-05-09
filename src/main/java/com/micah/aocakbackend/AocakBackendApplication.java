1aa97597a43fb3a78a3fad1b8fc0ddc76c83f58f
micah <1569124799@qq.com>
2023-05-09 17:24:54 +0800

    first commit

---
+++ src/main/java/com/micah/aocakbackend/AocakBackendApplication.java
@@ -0,0 +1,16 @@
+package com.micah.aocakbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Micah
+ */
+@SpringBootApplication
+public class AocakBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AocakBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/micah/aocakbackend/AocakBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micah.aocakbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AocakBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
